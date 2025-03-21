84430c18b695f619ce77fc24cbf5af85e000ef03
Tyiss Goodman <sticktyiss@gmail.com>
2025-03-20 22:25:38 -0700

    Added a copying text function along with a js file.

---
+++ index.js
@@ -0,0 +1,12 @@
+// For copying the text in an element
+function copyText(element) {
+  const text = element.innerText || element.textContent;
+  navigator.clipboard.writeText(text).then(() => {
+      element.innerText = "Copied!";
+      setTimeout(() => {
+          element.innerText = text;
+      }, 1500);
+  }).catch(err => {
+      console.error("Failed to copy text: ", err);
+  });
+}
